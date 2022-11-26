4541c09289b13ca6af31ac60fdea2e715f9e4fe8
viveksutar24 <114797135+viveksutar24@users.noreply.github.com>
2022-11-26 13:33:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function submit() {
+    alert("Your Registration Form is Sumbitted 😀 !!!!");
+}
+function cancel() {
+    alert("Your Registration Form is Cancelled , PLEASE RE-ENTER THE FOLLOWING INFORMATION:");
+}
